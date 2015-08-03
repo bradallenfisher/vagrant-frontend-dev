f1bf5826e10b396e6b6c0e28deb82edd12acf30a
Brad Fisher <bradallenfisher@gmail.com>
2015-08-02 22:05:59 -0400

    Update root.sh

--- scripts/installer/root.sh
+++ scripts/installer/root.sh
@@ -2,7 +2,3 @@
 curl --silent --location https://rpm.nodesource.com/setup | bash -
 yum -y install nodejs -y
 yum groupinstall 'Development Tools'
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
-sudo npm install -g browsersync
