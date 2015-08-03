eec5527e44aed22201e9b164ab766003dade683d
Brad Fisher <bradallenfisher@gmail.com>
2015-08-02 21:37:30 -0400

    Update root.sh

--- scripts/installer/root.sh
+++ scripts/installer/root.sh
@@ -1,4 +1,5 @@
 #Get Node tools
-curl --silent --location https://rpm.nodesource.com/setup | bash -
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo rpm -Uvh epel-release-6*.rpm
 yum -y install nodejs -y
 yum groupinstall 'Development Tools' -y
