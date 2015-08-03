10998845074ad90f217a8ac1519ac2678dd46acd
Brad Fisher <bradallenfisher@gmail.com>
2015-08-02 21:17:42 -0400

    Update vagrant.sh

--- scripts/installer/vagrant.sh
+++ scripts/installer/vagrant.sh
@@ -3,8 +3,6 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 source /home/vagrant/.rvm/scripts/rvm
 rvm get head
 rvm install ruby
-mkdir -p /var/www/html
-cd /var/www/html
 git clone https://github.com/heyMP/frontend-dev.git
 sudo npm install -g bower
 sudo npm install -g gulp
